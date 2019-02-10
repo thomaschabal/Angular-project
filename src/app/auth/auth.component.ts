fef0247256a38f5a30e56589cbd099e255815d01
Thomas <t.chabal@hotmail.fr>
2019-02-10 18:34:03 +0100

    Animations de page Auth, affichage formulaire register et affichage cgu mis en place

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -9,22 +9,66 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss'],
   animations : [
-    trigger('backgroundAnimation1', [
-      state('visible', style({opacity: 1,  backgroundImage : 'url("../../assets/images/wei.jpg")'})),
-      state('hidden', style({opacity: 1,  backgroundImage : 'url("../../assets/images/font1.jpg")'})),
-      transition('* => *', [ animate(6000) ] )
-    ]),
-    trigger('backgroundAnimation2', [
-      state('visible', style({opacity: 1,  backgroundImage : 'url("../../assets/images/font1.jpg")'})),
-      state('hidden', style({opacity: 0,  backgroundImage : 'url("../../assets/images/font1.jpg")'})),
-      transition('* => *', [ animate(6000) ] )
+    trigger('backgroundAnimation', [
+      state('visible1', style({opacity: 0.5,
+                               transform: 'translateX(0)',
+                               backgroundImage : 'url("../../assets/images/wei.jpg")',
+                               backgroundPosition : 'center center',
+                               width: '120%'})),
+      state('hidden1', style({opacity: 0.2,
+                              transform: 'translateX(-14em)',
+                              backgroundImage : 'url("../../assets/images/wei.jpg")',
+                              backgroundPosition : 'center center',
+                              width: '120%'})),
+      state('visible2', style({opacity: 0.5,
+                               transform: 'translateX(0)',
+                               backgroundImage : 'url("../../assets/images/font1.jpg")',
+                               backgroundPosition : 'center center',
+                               width: '120%'})),
+      state('hidden2', style({opacity: 0.2,
+                              transform: 'translateX(-14em)',
+                              backgroundImage : 'url("../../assets/images/font1.jpg")',
+                              backgroundPosition : 'center center',
+                              width: '120%'})),
+      state('visible3', style({opacity: 0.5,
+                               transform: 'translateX(0)',
+                               backgroundImage : 'url("../../assets/images/pom.jpg")',
+                               backgroundPosition : 'center center',
+                               width: '120%'})),
+      state('hidden3', style({opacity: 0.2,
+                              transform: 'translateX(-14em)',
+                              backgroundImage : 'url("../../assets/images/pom.jpg")',
+                              backgroundPosition : 'center center',
+                              width: '120%'})),
+      state('visible4', style({opacity: 0.5,
+                               transform: 'translateX(0)',
+                               backgroundImage : 'url("../../assets/images/voyage1.jpg")',
+                               backgroundPosition : 'center center',
+                               width: '120%'})),
+      state('hidden4', style({opacity: 0.2,
+                              transform: 'translateX(-14em)',
+                              backgroundImage : 'url("../../assets/images/voyage1.jpg")',
+                              backgroundPosition : 'center center',
+                              width: '120%'})),
+      transition('visible1 => hidden1', [ animate(6000) ] ),
+      transition('visible2 => hidden2', [ animate(6000) ] ),
+      transition('visible3 => hidden3', [ animate(6000) ] ),
+      transition('visible4 => hidden4', [ animate(6000) ] ),
+      transition('hidden1 => visible2', [ animate(0) ] ),
+      transition('hidden2 => visible3', [ animate(0) ] ),
+      transition('hidden3 => visible4', [ animate(0) ] ),
+      transition('hidden4 => visible1', [ animate(0) ] )
     ])
   ]
 })
 export class AuthComponent implements OnInit {
 
-  state1 = "visible";
-  state2 = "hidden";
+  state = "visible1";
+  indState = 0;
+  possibleStates = ["visible1", "hidden1",
+                    "visible2", "hidden2",
+                    "visible3", "hidden3",
+                    "visible4", "hidden4"];
 
   userForm : FormGroup;
 
@@ -43,12 +87,14 @@ export class AuthComponent implements OnInit {
     });
   }
 
-  changeFond1() {
-    this.state1 = this.state1 === "visible" ? "hidden" : "visible";
-  }
-
-  changeFond2() {
-    this.state2 = this.state2 === "visible" ? "hidden" : "visible";
+  changeFond() {
+    //this.state1 = this.state1 === "visible" ? "hidden" : "visible";
+    if (this.indState !== this.possibleStates.length) {
+      this.indState += 1;
+    } else {
+      this.indState = 0;
+    }
+    this.state = this.possibleStates[this.indState];
   }
 
   onSignIn() {
--- src/app/reset/reset.component.ts
+++ src/app/reset/reset.component.ts
@@ -2,13 +2,73 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HttpService } from '../services/http.service';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-reset',
   templateUrl: './reset.component.html',
-  styleUrls: ['./reset.component.scss']
+  styleUrls: ['./reset.component.scss'],
+  animations : [
+    trigger('backgroundAnimation', [
+      state('visible1', style({opacity: 0.5,
+                               transform: 'translateX(0)',
+                               backgroundImage : 'url("../../assets/images/wei.jpg")',
+                               backgroundPosition : 'center center',
+                               width: '120%'})),
+      state('hidden1', style({opacity: 0.2,
+                              transform: 'translateX(-14em)',
+                              backgroundImage : 'url("../../assets/images/wei.jpg")',
+                              backgroundPosition : 'center center',
+                              width: '120%'})),
+      state('visible2', style({opacity: 0.5,
+                               transform: 'translateX(0)',
+                               backgroundImage : 'url("../../assets/images/font1.jpg")',
+                               backgroundPosition : 'center center',
+                               width: '120%'})),
+      state('hidden2', style({opacity: 0.2,
+                              transform: 'translateX(-14em)',
+                              backgroundImage : 'url("../../assets/images/font1.jpg")',
+                              backgroundPosition : 'center center',
+                              width: '120%'})),
+      state('visible3', style({opacity: 0.5,
+                               transform: 'translateX(0)',
+                               backgroundImage : 'url("../../assets/images/pom.jpg")',
+                               backgroundPosition : 'center center',
+                               width: '120%'})),
+      state('hidden3', style({opacity: 0.2,
+                              transform: 'translateX(-14em)',
+                              backgroundImage : 'url("../../assets/images/pom.jpg")',
+                              backgroundPosition : 'center center',
+                              width: '120%'})),
+      state('visible4', style({opacity: 0.5,
+                               transform: 'translateX(0)',
+                               backgroundImage : 'url("../../assets/images/voyage1.jpg")',
+                               backgroundPosition : 'center center',
+                               width: '120%'})),
+      state('hidden4', style({opacity: 0.2,
+                              transform: 'translateX(-14em)',
+                              backgroundImage : 'url("../../assets/images/voyage1.jpg")',
+                              backgroundPosition : 'center center',
+                              width: '120%'})),
+      transition('visible1 => hidden1', [ animate(6000) ] ),
+      transition('visible2 => hidden2', [ animate(6000) ] ),
+      transition('visible3 => hidden3', [ animate(6000) ] ),
+      transition('visible4 => hidden4', [ animate(6000) ] ),
+      transition('hidden1 => visible2', [ animate(0) ] ),
+      transition('hidden2 => visible3', [ animate(0) ] ),
+      transition('hidden3 => visible4', [ animate(0) ] ),
+      transition('hidden4 => visible1', [ animate(0) ] )
+    ])
+  ]
 })
 export class ResetComponent implements OnInit {
+
+  state = "visible1";
+  indState = 0;
+  possibleStates = ["visible1", "hidden1",
+                    "visible2", "hidden2",
+                    "visible3", "hidden3",
+                    "visible4", "hidden4"];
 
   resetForm : FormGroup;
 
@@ -24,6 +84,16 @@ export class ResetComponent implements OnInit {
     this.resetForm = this.formBuilder.group({
       email : ['', Validators.required]
     });
+  }
+
+  changeFond() {
+    //this.state1 = this.state1 === "visible" ? "hidden" : "visible";
+    if (this.indState !== this.possibleStates.length) {
+      this.indState += 1;
+    } else {
+      this.indState = 0;
+    }
+    this.state = this.possibleStates[this.indState];
   }
 
   onSubmitForm() {
