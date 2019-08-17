25cb712a142f3b8b5900c98bca71b91c212859a7
thomaschabal <t.chabal@hotmail.fr>
2019-08-17 15:11:12 +0200

    Remove other assets

--- src/app/services/home.service.ts
+++ src/app/services/home.service.ts
@@ -12,27 +12,27 @@ export class HomeService {
   lovePics = [
     {
       title : 'Rugby',
-      address : './assets/images/sport4.jpg'
+      address : './assets/images/love_pics/sport4.jpg'
     },
     {
       title : 'OB de Février',
-      address : './assets/images/art_2018.jpg'
+      address : './assets/images/love_pics/art_2018.jpg'
     },
     {
       title : 'Sushis à la Skult',
-      address : './assets/images/skult_sushis.JPG'
+      address : './assets/images/love_pics/skult_sushis.JPG'
     },
     {
       title : 'Dimanche de lancement de campagne BDE',
-      address : './assets/images/dim_camp_2017.JPG'
+      address : './assets/images/love_pics/dim_camp_2017.JPG'
     },
     {
       title : 'Club Rock 2018',
-      address : './assets/images/rock2018.jpg'
+      address : './assets/images/love_pics/rock2018.jpg'
     },
     {
       title : 'Nuit de la Rentrée 2017',
-      address : './assets/images/soiree2.jpg'
+      address : './assets/images/love_pics/soiree2.jpg'
     }
   ];
 
