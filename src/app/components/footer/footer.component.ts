82b7b101bec58fc1e3337ca800f91c016a3454e8
Thomas <thomas.chabal@hotmail.fr>
2019-10-07 00:37:44 +0200

    Removed LinkedIn link and adjust css reset page

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -12,8 +12,34 @@ import { Phrases } from '../../Phrases';
 })
 
 export class FooterComponent {
-
   phrases: object;
+  links = [
+    {
+      href: 'https://www.facebook.com/club.ponthe',
+      class: 'fa-facebook',
+      name: 'Facebook',
+    },
+    {
+      href: 'https://upont.enpc.fr/assos/ponthe',
+      class: 'fa-instagram',
+      name: 'uPont',
+    },
+    {
+      href: 'https://github.com/ENPC-Ponthe/Angular-project',
+      class: 'fa-github',
+      name: 'GitHub',
+    },
+    // {
+    //   href: '#',
+    //   class: 'fa-linkedin',
+    //   name: 'LinkedIn',
+    // },
+    {
+      href: 'mailto:ponthe@liste.enpc.fr',
+      class: 'fa-envelope-o',
+      name: 'Email',
+    },
+  ];
 
   constructor(private httpService: HttpService,
               private router: Router) {
