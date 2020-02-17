786ceb827b229de3451751bf6a85f28fef98e9d4
Thomas <t.chabal@hotmail.fr>
2020-02-17 17:55:58 +0100

    Remove top white banner from reset page

--- src/app/pages/event/event.component.spec.ts
+++ src/app/pages/event/event.component.spec.ts
@@ -1,11 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
 
 import { EventComponent } from './event.component';
-import { GaleriesContactFormComponent } from '../../components/galeries-contact-form/galeries-contact-form.component';
-import { GaleriesFooterComponent } from '../../components/galeries-footer/galeries-footer.component';
-import { GaleriesModerationButtonsComponent } from '../../components/galeries-moderation-buttons/galeries-moderation-buttons.component';
+import { GaleriesContactFormComponent } from '../../components/galeries/galeries-contact-form/galeries-contact-form.component';
+import { GaleriesFooterComponent } from '../../components/galeries/galeries-footer/galeries-footer.component';
+import {
+  GaleriesModerationButtonsComponent
+} from '../../components/galeries/galeries-moderation-buttons/galeries-moderation-buttons.component';
 import { ImageViewerComponent } from '../../components/image-viewer/image-viewer.component';
 import { LoadingSpinnerComponent } from '../../components/loaders/loading-spinner/loading-spinner.component';
 import { UploadComponent } from '../../components/upload/upload.component';
