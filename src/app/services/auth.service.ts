0f17f0ce920818ed69cfe1b97dfe65d1941a10f4
Thomas <t.chabal@hotmail.fr>
2020-02-18 23:02:56 +0100

    Debug SSO #1

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -93,6 +93,8 @@ export class AuthService {
     this.httpService.getV1(API_ROUTES.casLogin + '?ticket=' + ticket).subscribe(() => {});
   }
   casAuthentication(ticket: string) {
+    this.httpService.get('/cas/login' + '?ticket=' + ticket).toPromise()
+    .then(() =>
     this.httpService.get(API_ROUTES.casAuthenticate + '?ticket=' + ticket).subscribe(
       (res: { access_token }) => {
         this.authenticate(res.access_token);
@@ -100,7 +102,7 @@ export class AuthService {
       (error) => {
         this.loginError = true;
       }
-    );
+    ));
   }
 
   casProcess(ticket: string) {
